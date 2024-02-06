5e8e1a87d9ec0523742fb85611e8a5605d6d0f02
Netotest <sakamotokenshin@yandex.ru>
2024-02-06 04:44:12 +0300

    task completed

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int balance = 50;
+        int transfer = 999;
+        int bonus;
+        if (transfer > 1000) {
+            bonus = transfer / 100;
+        } else bonus = 0;
+        if (transfer > 1000) {
+            System.out.println("Итоговый счёт: " + (balance + transfer + bonus));
+            System.out.println("Бонусные рубли: " + (bonus));
+        } else {
+            System.out.println("Итоговый счёт: " + (balance + transfer + bonus));
+            System.out.println("Бонусные рубли: " + (bonus));
         }
     }
 }
