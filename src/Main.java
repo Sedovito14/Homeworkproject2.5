0e5922ac6f50c4ab697351ca8221954032feb217
Netotest <sakamotokenshin@yandex.ru>
2024-02-06 18:30:57 +0300

    correction

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,14 @@ public class Main {
     public static void main(String[] args) {
 
         int balance = 50;
-        int transfer = 999;
+        int transfer = 1110;
         int bonus;
         if (transfer > 1000) {
             bonus = transfer / 100;
-        } else bonus = 0;
-        if (transfer > 1000) {
-            System.out.println("Итоговый счёт: " + (balance + transfer + bonus));
-            System.out.println("Бонусные рубли: " + (bonus));
         } else {
-            System.out.println("Итоговый счёт: " + (balance + transfer + bonus));
-            System.out.println("Бонусные рубли: " + (bonus));
+            bonus = 0;
         }
+        System.out.println("Итоговый счёт: " + (balance + transfer + bonus));
+        System.out.println("Бонусные рубли: " + (bonus));
     }
 }
